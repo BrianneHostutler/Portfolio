f8edfed8c748b462051c22c27448adf1f17a5986
Brianne <briannehostutler@gmail.com>
2017-05-23 20:45:04 -0400

    minify js

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,1 @@
-$(document).ready(function(){
-console.log ("Hi!");
-
-  $(window).scroll(function(){
-      var scrolled= $(document).scrollTop();
-      if (scrolled > 50) {
-        // $('.navWrapper').css({"background-color":"white"});
-        // $('#navigation a').css({"color":"black"});
-      $(".navWrapper").addClass("Stage2");
-      }
-      else if (scrolled < 50) {
-       $(".navWrapper").removeClass("Stage2");
-      } else {
-      }
-  });
-
-});
+$(document).ready(function(){console.log("Hi!"),$(window).scroll(function(){var a=$(document).scrollTop();a>50?$(".navWrapper").addClass("Stage2"):a<50&&$(".navWrapper").removeClass("Stage2")})});
